88845d4f801144830316be3b8bef2d32848bed81
Muhamad Fihris Aldama <fihrisaldama015@gmail.com>
2024-12-19 08:07:35 +0700

    [add] order detail

--- adminpanel/orderitems.php
+++ adminpanel/orderitems.php
@@ -75,60 +75,65 @@
                 </li>
             </ol>
         </nav>
+        <div class="contents">
+            <h2>Item Pesanan</h2>
+            <table width="100%" border="1" cellspacing="0" cellpadding="0">
+                <tr>
+                    <th scope="col">Nama Produk</th>
+                    <th scope="col">Username </th>
+                    <th scope="col">Contact</th>
+                    <th scope="col">Address</th>
+                    <th scope="col">Jumlah </th>
+                    <th scope="col">Sub </th>
+                </tr>
+                <?php
+
+                // Display order items
+                if (isset($_GET['oid'])) {
+                    $oid = $_GET['oid'];
+
+                    $sql = "SELECT product.productname, orderitem.qty, orderitem.item_price, user.username, user.contact_no, user.address 
+                            FROM orderitem 
+                            INNER JOIN product ON orderitem.pid = product.pid
+                            INNER JOIN user ON orderitem.uid = user.uid 
+                            WHERE orderitem.oid = ? 
+                            ORDER BY orderitem.oiid DESC";
+
+                    $stmt = $mysqli->prepare($sql);
+                    $stmt->bind_param("i", $oid);
+                    $stmt->execute();
+                    $result = $stmt->get_result();
+
+                    if ($result->num_rows > 0) {
+                        while ($row = $result->fetch_assoc()) {
+                            $quantity = (int) $row['qty'];
+                            $price = (float) $row['item_price'];
+                            $subtotal = $quantity * $price;
+
+                            echo "<tr>";
+                            echo "<td>" . htmlspecialchars($row['productname']) . "</td>";
+                            echo "<td>" . htmlspecialchars($row['username']) . "</td>";
+                            echo "<td>" . htmlspecialchars($row['contact_no']) . "</td>";
+                            echo "<td>" . htmlspecialchars($row['address']) . "</td>";
+                            echo "<td>" . htmlspecialchars($quantity) . "</td>";
+                            echo "<td>Rp. " . htmlspecialchars(number_format($subtotal, 2)) . "</td>";
+                            echo "</tr>";
+                        }
+                    } else {
+                        echo "<tr><td colspan=\"3\">Tidak ada item pesanan!</td></tr>";
+                    }
+
+                    $stmt->close();
+                } else {
+                    echo "<tr><td colspan=\"3\">ID Pesanan tidak tersedia!</td></tr>";
+                }
+
+
+                ?>
+            </table>
+
+        </div>
     </div>
-
-    <div class="contents">
-				<h2>Item Pesanan</h2>
-				<table width="100%" border="1" cellspacing="0" cellpadding="0">
-					<tr>
-						<th scope="col">Nama Produk </th>
-						<th scope="col">Jumlah </th>
-						<th scope="col">Sub </th>
-					</tr>
-					<?php
-
-					// Display order items
-					if (isset($_GET['oid'])) {
-						$oid = $_GET['oid'];
-
-						$sql = "SELECT product.productname, orderitem.qty, orderitem.item_price 
-								FROM orderitem 
-								INNER JOIN product ON orderitem.pid = product.pid 
-								WHERE orderitem.oid = ? 
-								ORDER BY orderitem.oiid DESC";
-
-						$stmt = $mysqli->prepare($sql);
-						$stmt->bind_param("i", $oid);
-						$stmt->execute();
-						$result = $stmt->get_result();
-
-						if ($result->num_rows > 0) {
-							while ($row = $result->fetch_assoc()) {
-								$quantity = (int) $row['qty'];
-								$price = (float) $row['item_price'];
-								$subtotal = $quantity * $price;
-
-								echo "<tr>";
-								echo "<td>" . htmlspecialchars($row['productname']) . "</td>";
-								echo "<td>" . htmlspecialchars($quantity) . "</td>";
-								echo "<td>Rp. " . htmlspecialchars(number_format($subtotal, 2)) . "</td>";
-								echo "</tr>";
-							}
-						} else {
-							echo "<tr><td colspan=\"3\">Tidak ada item pesanan!</td></tr>";
-						}
-
-						$stmt->close();
-					} else {
-						echo "<tr><td colspan=\"3\">ID Pesanan tidak tersedia!</td></tr>";
-					}
-
-
-					?>
-				</table>
-
-			</div>
-
 
 <script src="../bootstrap/js/bootstrap.bundle.min.js"></script>
 <script src="../fontawesome/js/all.min.js"></script>
